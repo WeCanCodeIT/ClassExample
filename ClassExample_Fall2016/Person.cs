ead9241f2f4aa7abb00f9ef0f845ba0f48852eb0
Jarryd Huntley <jarryd.huntley@gmail.com>
2016-09-15 13:09:01 -0400

    Added in some fields.

---
+++ ClassExample_Fall2016/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample_Fall2016
+{
+    class Person
+    {
+        private int age;
+        private string name;
+
+        private string eyeColor;
+
+        public string greeting;
+
+    }
+}
