da8c2cbc0971f3774e147f76c14c2a6c7ddc1269
Jarryd Huntley <jarryd.huntley@gmail.com>
2016-09-16 10:02:25 -0400

    Added in the start of the student class.

---
+++ ClassExample_Fall2016/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample_Fall2016
+{
+    class Student
+    {
+        //Fields
+        private string fullName;
+        private int age;
+        private string university;
+        private string major;
+        private float gpa;       
+
+        //Properties
+
+        //Constructors
+        public Student()
+        {
+
+        }
+
+        //Methods
+    }
+}
